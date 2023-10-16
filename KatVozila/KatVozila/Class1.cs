1aa779d8f099fd1cd66a6deb76c4b9e64aefc066
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-10-16 10:05:17 +0200

    Add files via upload

---
+++ KatVozila/KatVozila/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatVozila
+{
+    class Vozila
+    {
+        string model, godiste, brKotaca;
+        string kategorija;
+        
+
+        public Vozila(string model,string godiste, string brKotaca,string kategorija)
+        { 
+            this.model = model;
+            this.godiste = godiste;
+            this.brKotaca = brKotaca;
+            this.kategorija = kategorija;
+            
+        }
+
+        public string Model { get=>model; set=>model=value; }
+        public string Godiste { get=>godiste; set=>godiste=value; }
+        public string BrKotaca { get=>brKotaca; set=>brKotaca=value; }
+        public string Kategorija { get=>Kategorija; set=>Kategorija=value; }
+    }
+}
